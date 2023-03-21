5064abd31fca943ff8dbc9c2bcef9b4551e41a09
Mike2000160 <83926799+Mike2000160@users.noreply.github.com>
2023-03-21 00:08:35 -0400

    Added a 2nd Leve, Locked the camera
    Camera Locked so it does not go out of bounds.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     [SerializeField] private Transform player;
+    [SerializeField] private float rightX;
+    [SerializeField] private float leftX;
+    [SerializeField] private float topY;
+    [SerializeField] private float bottomY;
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
+        //transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
+        transform.position = new Vector3(Mathf.Clamp(player.position.x, leftX, rightX), 
+                                            Mathf.Clamp(player.position.y, bottomY, topY), transform.position.z);
     }
 }

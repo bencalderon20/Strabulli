ecbd60fb3904d6f90bfbfe8f1840ffc97a7760d7
zp1k3y <benaltman1@gmail.com>
2023-03-25 19:27:16 -0400

    Added Jumping Mechanic to Enemies
    Plum now jumps, looks very nice

--- Assets/Scripts/enemyPatrol.cs
+++ Assets/Scripts/enemyPatrol.cs
@@ -17,15 +17,17 @@ public class enemyPatrol : MonoBehaviour
 
     [SerializeField] private  int health = 100;
     [SerializeField] private GameObject deathEffect;
+    [SerializeField] private float jump = 7f;
+    [SerializeField] private LayerMask jumpableGround;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        //anim = GetComponent<Animator>();
+        anim = GetComponent<Animator>();
         coll = GetComponent<BoxCollider2D>();
         currentPoint = rightEdge.transform;
-        //anim.SetBool("moving", true);
+        anim.SetBool("moving", true);
     }
 
     public void TakeDamage(int damage)
@@ -33,12 +35,14 @@ public class enemyPatrol : MonoBehaviour
         health -= damage;
         if (health <= 0)
         {
+            //Debug.Log("Ded");
             StartCoroutine(Die());
         }
     }
 
     IEnumerator Die()
     {    
+        //Debug.Log("Super Ded");
         anim.SetTrigger("death");
         coll.enabled = !coll.enabled;
         yield return new WaitForSeconds(0.5f);
@@ -53,22 +57,27 @@ public class enemyPatrol : MonoBehaviour
 
         if(currentPoint == rightEdge.transform)
         {
-            rb.velocity = new Vector2(speed, 0);
+            rb.velocity = new Vector2(speed, rb.velocity.y);
         }
         else
         {
-            rb.velocity = new Vector2(-speed, 0);
+            rb.velocity = new Vector2(-speed, rb.velocity.y);
         }
 
-        if(Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == rightEdge.transform)
+        if(Mathf.Abs(transform.position.x - currentPoint.position.x) < 0.5f && currentPoint == rightEdge.transform)
         {
             flip();
             currentPoint = leftEdge.transform;
         }
-        if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == leftEdge.transform)
+        if (Mathf.Abs(transform.position.x - currentPoint.position.x) < 0.5f && currentPoint == leftEdge.transform)
         {
             flip();
             currentPoint = rightEdge.transform;
+        }
+
+        if (IsGrounded() && jump > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jump);
         }
     }
 
@@ -77,5 +86,10 @@ public class enemyPatrol : MonoBehaviour
         Vector3 localScale = transform.localScale;
         localScale.x *= -1;
         transform.localScale = localScale;
+    }
+
+    private bool IsGrounded()
+    {
+       return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
     }
 }

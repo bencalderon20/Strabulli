acdd3d11ca62aa6a86ad34cd8d7b85babb0f1af3
bencalderon20 <bcalderon@umassd.edu>
2023-03-31 14:21:26 -0400

    Grapefruit Animate
    Grapefruit can nows player an animation when firing Grapes

--- Assets/Scripts/EnemyShoot.cs
+++ Assets/Scripts/EnemyShoot.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+#include <stdlib.h>
 
 public class EnemyShoot : MonoBehaviour
 {
@@ -8,24 +9,33 @@ public class EnemyShoot : MonoBehaviour
     public Transform bulletPos;
     private GameObject player;
     private float timer;
+    private Animator anim;
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         float distance = Vector2.Distance(transform.position, player.transform.position);
-        if (distance < 5)
+        if (abs(distance) < 5)
         {
+            anim.SetBool("Attack", true);
             timer += Time.deltaTime;
-            if (timer > 5)
+            if (timer > 2)
             {
                 timer = 0;
                 shoot();
+                anim.SetBool("Attack", false);
             }
+        }
+        else
+        {
+            timer = 0;
+            anim.SetBool("Attack", false);
         }
     }
     void shoot()

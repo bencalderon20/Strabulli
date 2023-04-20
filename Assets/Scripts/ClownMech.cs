d81ddef58594e26c323c29ae742ba6f491c85ff3
bencalderon20 <bcalderon@umassd.edu>
2023-04-20 16:37:03 -0400

    Clown fires more projectiles
    Fixed the behavior of the purple  tomato to roll, the white tomato floats around, the orange tomato bounces across the ground, the mech can shoot a tomato sauce projectile from its nose ,and it can dash towards the player.
    Need to make the tomato sauce fire in a stream.
    Changed the tomato sauce sprite and moved some scripts into the script folder.

--- Assets/Scripts/ClownMech.cs
+++ Assets/Scripts/ClownMech.cs
@@ -12,13 +12,18 @@ public class ClownMech : MonoBehaviour
     private Material originalMaterial;
     private Coroutine flashRoutine;
 
-    public GameObject purple;
-    public GameObject white;
+    [SerializeField] private GameObject purple;
+    [SerializeField] private GameObject white;
+    [SerializeField] private GameObject orange;
+    [SerializeField] private GameObject sauce;
+    private GameObject clown;
     public Transform bulletPos;
+    public Transform saucePos;
     private GameObject player;
     private int attack;
     private float timer=0; //This timer will allow the mech to walk for a bit before attack
     private float timer2=0; //This timer will allow the selection animation to play before the mech attacks
+    private bool dash = false;
     System.Random random = new System.Random(); //This will allow the mech to "randomly" select which attack it will use
 
     [SerializeField] private float speed;
@@ -31,6 +36,7 @@ public class ClownMech : MonoBehaviour
 
     [SerializeField] private GameObject rightEdge;
     [SerializeField] private GameObject leftEdge;
+    [SerializeField] private GameObject dashEdge;
 
     // Start is called before the first frame update
     void Start()
@@ -42,52 +48,96 @@ public class ClownMech : MonoBehaviour
         player = GameObject.FindGameObjectWithTag("Player");
         currentPoint = rightEdge.transform;
         originalMaterial = spr.material;
+
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (currentPoint == rightEdge.transform)
+        if (dash==false)
         {
-            rb.velocity = new Vector2(speed, rb.velocity.y);
-        }
-        else
-        {
-            rb.velocity = new Vector2(-speed, rb.velocity.y);
-        }
-        if (Mathf.Abs(transform.position.x - currentPoint.position.x) < 0.5f && currentPoint == rightEdge.transform)
-        {
-            currentPoint = leftEdge.transform;
-        }
-        if (Mathf.Abs(transform.position.x - currentPoint.position.x) < 0.5f && currentPoint == leftEdge.transform)
-        {
-            currentPoint = rightEdge.transform;
-        }
-        if(timer>=10)
-        {
-            /*anim.SetBool("Selecting", true);
-            timer2 += Time.deltaTime;
-            if(timer2>20)
+            if (currentPoint == rightEdge.transform)
             {
-                //anim.SetBool("Shoot", true);*/
-                attack = random.Next(2);
+                rb.velocity = new Vector2(speed, rb.velocity.y);
+            }
+            else
+            {
+                rb.velocity = new Vector2(-speed, rb.velocity.y);
+            }
+            if (Mathf.Abs(transform.position.x - currentPoint.position.x) < 0.5f && currentPoint == rightEdge.transform)
+            {
+                currentPoint = leftEdge.transform;
+            }
+            if (Mathf.Abs(transform.position.x - currentPoint.position.x) < 0.5f && currentPoint == leftEdge.transform)
+            {
+                currentPoint = rightEdge.transform;
+            }
+            if (timer >= 10)
+            {
+                /*anim.SetBool("Selecting", true);
+                timer2 += Time.deltaTime;
+                if(timer2>20)
+                {
+                    //anim.SetBool("Shoot", true);*/
+                attack = random.Next(5);
                 switch (attack)
                 {
                     case 0:
                         //anim.SetBool("Selecting", false);
-                        Instantiate(purple, bulletPos.position, Quaternion.identity);
+                        Instantiate(white, bulletPos.position, Quaternion.identity);
                         break;
                     case 1:
                         //anim.SetBool("Selecting", false);
-                        Instantiate(white, bulletPos.position, Quaternion.identity);
+                        Instantiate(purple, bulletPos.position, Quaternion.identity);
+                        break;
+                    case 2:
+                        //anim.SetBool("Selecting", false);
+                        Instantiate(orange, bulletPos.position, Quaternion.identity);
+                        break;
+                    case 3:
+                        
+                        break;
+                    case 4:
+                        dash = true;
+                        currentPoint = dashEdge.transform;
                         break;
                 }
                 /*timer2 = 0;
             }
             
-           */ timer = 0;
+           */
+                timer = 0;
+            }
+            timer += Time.deltaTime;
         }
-        timer += Time.deltaTime;
+        else
+        {
+            if (currentPoint == dashEdge.transform)
+            {
+                if (timer2 > 5)
+                {
+                    rb.velocity = new Vector2(-speed * 4, rb.velocity.y);
+                }
+                else
+                    rb.velocity = new Vector2(0, rb.velocity.y);
+                timer2 += Time.deltaTime;
+            }
+            else
+            {
+                rb.velocity = new Vector2(speed * 1.5f, rb.velocity.y);
+            }
+            if (Mathf.Abs(transform.position.x - currentPoint.position.x) < 0.6f && currentPoint == dashEdge.transform)
+            {
+                timer2 = 0;
+                currentPoint = leftEdge.transform;
+            }
+            if (Mathf.Abs(transform.position.x - currentPoint.position.x) < 0.5f && currentPoint == leftEdge.transform)
+            {
+                dash = false;
+                currentPoint = rightEdge.transform;
+            }
+        }
     }
 
     public void TakeDamage(int damage)
@@ -137,5 +187,11 @@ public class ClownMech : MonoBehaviour
         //Instantiate(deathEffect, transform.position, Quaternion.identity);
         Destroy(gameObject);
     }
-
+    void ShootSauce()
+    {
+        float tme = 0;
+        if(tme%3==0)
+            Instantiate(sauce, saucePos.position, Quaternion.identity);
+        tme += Time.deltaTime;
+    }
 }

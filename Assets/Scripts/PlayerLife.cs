922ebbfc8d111b6ec39fca314efabaad0bb88dbc
Mike2000160 <83926799+Mike2000160@users.noreply.github.com>
2023-04-24 21:49:47 -0400

    changed spikes on level 3

--- Assets/Scripts/PlayerLife.cs
+++ Assets/Scripts/PlayerLife.cs
@@ -19,6 +19,7 @@ public class PlayerLife : MonoBehaviour
     [SerializeField] private  int health = 100;
     public int damage = 1;
     private float direction;
+    int hitCounter = 0;
 
     // Start is called before the first frame update
     private void Start()
@@ -83,6 +84,22 @@ public class PlayerLife : MonoBehaviour
         {
             TakeDamage(health, collision.transform);
         }
+        if(collision.gameObject.CompareTag("colliderDisabler"))
+        {
+            hitCounter = hitCounter+1;
+            if (hitCounter == 3)
+            {
+                StartCoroutine(colliderDisabling());
+            }
+        }
+    }
+
+    IEnumerator colliderDisabling()
+    {
+        GetComponent<Collider2D>().enabled = false;
+        yield return new WaitForSeconds(2);
+        GetComponent<Collider2D>().enabled = true;
+
     }
 
     private void Die()
